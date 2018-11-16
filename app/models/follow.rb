c7c8e55f99d699fdb68507177d5b5f074374678c
Raghu Betina <raghu@firstdraft.com>
2018-11-16 22:45:29 +0000

    Validate uniqueness of follows.stock_id

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -5,6 +5,8 @@ class Follow < ApplicationRecord
 
   # Validations
 
+  validates :stock_id, :uniqueness => { :scope => [:user_id], :message => "already liked" }
+
   validates :stock_id, :presence => true
 
   validates :user_id, :presence => true
